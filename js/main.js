59e059a5400a4a6ca64620960ce2be4c22053c95
abdunabiyevahror <axronabiyev@gmail.com>
2022-12-12 14:47:30 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+alert('Welcome')
+alert('accept to location ');
+alert("accept to gallery ");
+const myname = prompt(' Enter your name ');
+const phone = prompt(" Enter your email ");
+const year = prompt("Enter your hometown ");
+
+console.log(myname);
+console.log(phone);
+console.log(year);
